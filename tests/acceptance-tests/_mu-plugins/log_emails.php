4b041376973a532e5577d0c8664ebc4942a357ab
Steve Henty <stevehenty@gmail.com>
2018-06-03 09:24:53 +0200

    Change hook to gform_after_email

--- tests/acceptance-tests/_mu-plugins/log_emails.php
+++ tests/acceptance-tests/_mu-plugins/log_emails.php
@@ -9,17 +9,16 @@ Author URI: https://gravityflow.io
 License: GPL-2.0+
 */
 
-add_filter( 'gravityflow_notification', 'sh_gravityflow_notification', 10, 4 );
-function sh_gravityflow_notification ( $notification, $form, $entry, $step ) {
+add_action( 'gform_after_email', 'sh_gform_after_email', 10, 13 );
+function sh_gform_after_email ( $is_success, $to, $subject, $message, $headers, $attachments, $message_format, $from, $from_name, $bcc, $reply_to, $entry, $cc ) {
 	$page = array(
 		'post_type'    => 'post',
-		'post_content' => $notification['message'],
-		'post_name'    => sanitize_title_with_dashes( $notification['subject'] ),
+		'post_content' => $message,
+		'post_name'    => sanitize_title_with_dashes( $subject ),
 		'post_parent'  => 0,
 		'post_author'  => 1,
 		'post_status'  => 'publish',
-		'post_title'   => $notification['subject'],
+		'post_title'   => $subject,
 	);
 	wp_insert_post( $page );
-	return $notification;
 }
