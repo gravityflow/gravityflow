49031b69b286a3bfe7a0e581fca649a807ae2f3a
Richard Wawrzyniak <richard@wawrzyniak.me>
2018-10-02 19:56:25 +0100

    Added helpers

--- tests/unit-tests/test-inbox.php
+++ tests/unit-tests/test-inbox.php
@@ -125,4 +125,91 @@ class Tests_Gravity_Flow_Inbox extends GF_UnitTestCase {
 		$_GET['gflow_access_token'] = urldecode( gravity_flow()->generate_access_token( new Gravity_Flow_Assignee( 'email|test@test.test' ) ) );
 	}
 
+	/**
+	 * Returns a basic inbox page arguments array.
+	 *
+	 * @return array
+	 */
+	public function _get_form_id_args() {
+		return array( 'form_id' => $this->form_id );
+	}
+
+	/**
+	 * Used to test the filters.
+	 *
+	 * @return array
+	 */
+	public function _get_bingo_array() {
+		return array( 'bingo' );
+	}
+
+	/**
+	 * Creates an Approval type step.
+	 *
+	 * @param array $override_settings The additional step settings.
+	 *
+	 * @return mixed
+	 */
+	public function _add_approval_step( $override_settings = array() ) {
+		$default_settings = array(
+			'step_name'                               => 'Approval',
+			'description'                             => '',
+			'step_type'                               => 'approval',
+			'feed_condition_logic_conditional_logic'  => false,
+			'feed_condition_conditional_logic_object' => array(),
+			'type'                                    => 'select',
+			'assignees'                               => array( 'user_id|1' ),
+			'routing'                                 => array(),
+			'unanimous_approval'                      => '',
+			'assignee_notification_enabled'           => false,
+			'assignee_notification_message'           => 'A new entry is pending your approval',
+			'destination_complete'                    => 'next',
+			'destination_rejected'                    => 'complete',
+			'destination_approved'                    => 'next',
+		);
+
+		$settings = wp_parse_args( $override_settings, $default_settings );
+
+		return $this->api->add_step( $settings );
+	}
+
+	/**
+	 * Returns a random entry (not saved).
+	 *
+	 * @return array
+	 */
+	public function _generate_random_entry() {
+		$form = $this->factory->form->get_form_by_id( $this->form_id );
+
+		$random_entry_object            = $this->factory->entry->generate_random_entry_object( $form );
+		$random_entry_object['form_id'] = $form['id'];
+
+		return $random_entry_object;
+	}
+
+	/**
+	 * Saves ten entries to the database and returns their IDs.
+	 *
+	 * @return array
+	 */
+	public function _create_entries() {
+		return $this->factory->entry->create_many( 10, $this->_generate_random_entry() );
+	}
+
+	/**
+	 * Saves ten entries to the database, assigns them, and returns their IDs.
+	 *
+	 * @param string $filter_key The key which will be used in the search criteria when getting the entries later.
+	 *
+	 * @return array
+	 */
+	public function _create_assigned_entries( $filter_key ) {
+		$entry_ids = $this->_create_entries();
+		foreach ( $entry_ids as $entry_id ) {
+			gform_update_meta( $entry_id, $filter_key, 'pending' );
+		}
+
+		return $entry_ids;
+	}
+
 }
