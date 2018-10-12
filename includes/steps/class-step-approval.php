779dd4162cd4e0923a444d68e51b5816229c4beb
Idealien <jamie@idealienstudios.com>
2018-10-12 19:23:26 -0400

    Filter should only occur when revert email is defined

--- includes/steps/class-step-approval.php
+++ includes/steps/class-step-approval.php
@@ -582,8 +582,10 @@ class Gravity_Flow_Step_Approval extends Gravity_Flow_Step {
 				$note = $this->get_name() . ': ' . esc_html__( 'Reverted to step', 'gravityflow' ) . ' - ' . $step->get_label();
 				$this->add_note( $note . $this->maybe_add_user_note(), true );
 
-				//Determine whether the User Input assignee notification should send if an approval revert notification exists (Default: false)
-				add_filter( 'gravityflow_notification', array( $this, 'gravityflow_user_input_notification_send_on_revert' ), 10, 4 );
+				//Determine whether the revert notification is set and whether the user input notification should be sent or not as a result
+				if( $this->{'revert_notification_enabled'} ) {
+					add_filter( 'gravityflow_notification', array( $this, 'gravityflow_user_input_notification_send_on_revert' ), 10, 4 );
+				}
 
 				$step->start();
 				$feedback = esc_html__( 'Reverted to step:', 'gravityflow' ) . ' ' . $step->get_label();
