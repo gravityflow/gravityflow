e523179541b6c57473397429a75f44ad961cfe97
Yoren Chang <yorenchang@gmail.com>
2018-11-21 22:06:52 +0800

    Revert "Remove the <p> tag detection"
    This reverts commit e13ce9a11f9889ca0ebef0b4fde06a3813517db5.

--- class-gravity-flow.php
+++ class-gravity-flow.php
@@ -4318,6 +4318,9 @@ PRIMARY KEY  (id)
 
 					$entry = GFAPI::get_entry( $entry_id ); // Refresh entry.
 
+					if ( substr( $feedback, 0, 3 ) !== '<p>' ) {
+						$feedback = sprintf( '<p>%s</p>', $feedback );
+					}
 					?>
 					<div class="gravityflow_workflow_notice updated notice notice-success is-dismissible" style="padding:6px;">
 						<?php echo $feedback; ?>
