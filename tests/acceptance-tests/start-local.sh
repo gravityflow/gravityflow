3bd98d9512c93efe71bd491437a9d914da0eeabe
Steve Henty <stevehenty@gmail.com>
2018-11-25 19:05:41 +0100

    Put cd back

--- tests/acceptance-tests/start-local.sh
+++ tests/acceptance-tests/start-local.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# Run just one test
-#./vendor/bin/codecept run -c codeception-local.yml tests/acceptance-tests/acceptance/0038CurrentStepMergeTagCept.php -vvv --html --env win-chrome
+cd ../../
 
 # Run tests in a group.
 # Create groups by adding the following comment to the top of the tests
@@ -9,6 +8,10 @@
 #docker-compose run --rm codeception run -g myGroup -vvv --html
 
 docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm --name chrome selenium/standalone-chrome-debug:3.141.59-bismuth
+
+# Run just one test
+#./vendor/bin/codecept run -c codeception-local.yml tests/acceptance-tests/acceptance/0034ApprovalRevertCept.php -vvv --html
+
 # Run all tests
 ./vendor/bin/codecept run -c codeception-local.yml -vvv --html
 
