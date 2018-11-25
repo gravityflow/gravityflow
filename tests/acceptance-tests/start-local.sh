8610fc60ed21fd35eb027dbea8766efdce5058a3
Steve Henty <stevehenty@gmail.com>
2018-11-25 18:13:37 +0100

    Update selenlium-chrome

--- tests/acceptance-tests/start-local.sh
+++ tests/acceptance-tests/start-local.sh
@@ -8,7 +8,7 @@
 # // @group myGroup
 #docker-compose run --rm codeception run -g myGroup -vvv --html
 
-docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm --name chrome selenium/standalone-chrome-debug:3.141.0-actinium
+docker run -d -p 4444:4444 -p 5900:5900 -v /dev/shm:/dev/shm --name chrome selenium/standalone-chrome-debug:3.141.59-bismuth
 # Run all tests
 ./vendor/bin/codecept run -c codeception-local.yml -vvv --html
 
