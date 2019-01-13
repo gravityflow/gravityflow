bcef67b6989a903f5fb47026dc15805d6dd3fb61
Steve Henty <stevehenty@gmail.com>
2019-01-13 20:14:34 +0100

    Update class-step-update-user.php

--- includes/steps/class-step-update-user.php
+++ includes/steps/class-step-update-user.php
@@ -27,7 +27,7 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 	 *
 	 * @var string
 	 */
-	public $_step_type = 'update-user';
+	public $_step_type = 'update_user';
 
 	/**
 	 * Returns the step label.
@@ -48,11 +48,13 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 	 * @return string
 	 */
 	public function get_icon_url() {
-		return '<i class="fa fa-envelope-o"></i>';
+		return '<i class="fa fa-user"></i>';
 	}
 
 	/**
 	 * Returns an array of settings for this step type.
+	 *
+	 * @since 2.5
 	 *
 	 * @return array
 	 */
@@ -61,6 +63,48 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 			'title'       => esc_html__( 'Update User', 'gravityflow' ),
 			'description' => '',
 			'fields'      => array(
+				array(
+					'name'          => 'user_source',
+					'label'         => __( 'User', 'gravityflow' ),
+					'type'          => 'radio',
+					'default_value' => 'created_by',
+					'horizontal'    => false,
+					'onchange'      => 'jQuery(this).closest("form").submit();',
+					'choices'       => array(
+						array( 'label' => __( 'Form Submitter ("Created By" field)', 'gravityflow' ), 'value' => 'created_by' ),
+						array( 'label' => __( 'Current User', 'gravityflow' ), 'value' => 'current_user' ),
+						array( 'label' => __( 'Select User Field', 'gravityflow' ), 'value' => 'select_user_field' ),
+						array( 'label' => __( 'Lookup User by Email Field', 'gravityflow' ), 'value' => 'select_email_field' ),
+					),
+				),
+				array(
+					'name'          => 'user_field',
+					'label'         => __( 'User Field', 'gravityflow' ),
+					'type'          => 'field_select',
+					'args'        => array(
+						'input_types' => array( 'workflow_user' ),
+					),
+					'dependency' => array(
+						'field'  => 'user_source',
+						'values' => array( 'select_user_field' ),
+					)
+				),
+				array(
+					'name'          => 'email_field',
+					'label'         => __( 'Email Field', 'gravityflow' ),
+					'type'          => 'field_select',
+					'args'        => array(
+						'disable_first_choice' => true,
+						'input_types' => array( 'email' ),
+						'append_choices'       => array(
+							array( 'label' => __( 'Select an Email Field', 'gravityflow' ), 'value' => '' ),
+						),
+					),
+					'dependency' => array(
+						'field'  => 'user_source',
+						'values' => array( 'select_email_field' ),
+					)
+				),
 				array(
 					'name'     => 'first_name',
 					'label'    => esc_html__( 'First Name', 'gravityflow' ),
@@ -113,12 +157,14 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 					'tooltip'  => sprintf( '<h6>%s</h6> %s', esc_html__( 'Nickname', 'gravityflow' ), esc_html__( 'Select the form field that should be used for the user\'s nickname.', 'gravityflow' ) )
 				),
 				array(
-					'name'     => 'displayname',
-					'label'    => esc_html__( 'Display Name', 'gravityflow' ),
-					'type'     => 'select',
-					'class'    => 'medium',
-					'choices'  => $this->get_display_name_choices(),
-					'tooltip'  => sprintf( '<h6>%s</h6> %s', esc_html__( 'Display Name', 'gravityflow' ), esc_html__( 'Select how the user\'s name should be displayed publicly.', 'gravityflow' ) )
+					'name'          => 'display_name',
+					'id'            => 'display_name_setting', // Prevents conflict with WP class
+					'label'         => esc_html__( 'Display Name', 'gravityflow' ),
+					'type'          => 'select',
+					'class'         => 'medium',
+					'default_value' => '',
+					'choices'       => $this->get_display_name_choices(),
+					'tooltip'       => sprintf( '<h6>%s</h6> %s', esc_html__( 'Display Name', 'gravityflow' ), esc_html__( 'Select how the user\'s name should be displayed publicly.', 'gravityflow' ) ),
 				),
 				array(
 					'name'        => 'email',
@@ -127,7 +173,12 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 					'args'        => array(
 						'disable_first_choice' => true,
 						'input_types'          => array( 'email' ),
-						'append_choices'       => $this->get_extra_email_choice()
+						'append_choices'       => array(
+							array(
+								'label' =>  sprintf( '&mdash; %s &mdash;', esc_html__( 'Preserve current email', 'gravityflow'  ) ),
+								'value' => ''
+							)
+						)
 					),
 					'class'       => 'medium',
 					'tooltip'     => sprintf( '<h6>%s</h6> %s', esc_html__( 'Email Address', 'gravityflow' ), esc_html__( 'Select the form field that should be used for the user\'s email address.', 'gravityflow' ) )
@@ -140,7 +191,12 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 					'args'     => array(
 						'disable_first_choice' => true,
 						'input_types'          => array( 'password' ),
-						'append_choices'       => $this->get_extra_password_choices()
+						'append_choices'       => array(
+							array(
+								'label' => sprintf( '&mdash; %s &mdash;', esc_html__( 'Preserve current password', 'gravityflow'  ) ),
+								'value' => ''
+							)
+						)
 					),
 					'tooltip'  => sprintf( '<h6>%s</h6> %s', esc_html__( 'Password', 'gravityflow' ), esc_html__( 'Select the form field that should be used for the user\'s password.', 'gravityflow' ) )
 				),
@@ -180,16 +236,20 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 					)
 				),
 				array(
-					'name'      => 'userMeta',
+					'name'      => 'user_meta',
 					'label'     => esc_html__( 'User Meta', 'gravityflow' ),
 					'type'      => 'generic_map',
-					'enable_custom_key' => false,
-					'enable_custom_value' => true,
-					'key_field_title' => esc_html__( 'Field', 'gravityflow' ),
-					'value_field_title' => esc_html__( 'Value', 'gravityflow' ),
-					'key_choices' => $this->get_user_meta_choices(),
-					'value_choices' => $this->value_mappings(),
-					'class'     => 'medium'
+					'merge_tags'  => true,
+					'key_field'   => array(
+						'choices'     => $this->get_user_meta_choices(),
+						'placeholder' => esc_html__( 'Custom Meta Field', 'gravityflow' ),
+						'title'       => esc_html__( 'Meta Key', 'gravityflow' ),
+					),
+					'value_field' => array(
+						'choices'      => $this->value_mappings(),
+						'custom_value' => true,
+						'placeholder'  => esc_html__( 'Custom Field Value', 'gravityflow' ),
+					),
 				),
 			)
 		);
@@ -200,43 +260,60 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 	}
 
 	/**
-	 * Triggers sending of the selected notifications.
+	 * Processes the step and updates the user profile if the user can be found.
+	 *
+	 * @since 2.5
 	 *
 	 * @return bool
 	 */
 	function process() {
 		$this->log_debug( __METHOD__ . '(): starting' );
 
-		
+
+		$user = null;
+
+		switch( $this->user_source ) {
+			case 'created_by':
+				$entry = $this->get_entry();
+				if (  $entry['created_by'] ) {
+					$user = get_user_by( 'id', $entry['created_by'] );
+				}
+				break;
+			case 'current_user':
+				$user = wp_get_current_user();
+				break;
+			case 'select_user_field':
+				$entry = $this->get_entry();
+				if ( $entry[ $this->user_field ] ) {
+					$user = get_user_by( 'ID', $entry[ $this->user_field ] );
+				}
+				break;
+			case 'select_email_field':
+				$entry = $this->get_entry();
+				if ( $entry[ $this->email_field ] ) {
+					$user = get_user_by( 'email', $entry[ $this->user_field ] );
+				}
+		}
+
+		if ( ! $user ) {
+			$this->log_debug( __METHOD__ . '(): user not found. Bailing.' );
+		}
+
+		$this->set_user_properties( $user );
 
 		return true;
 	}
 
+	/**
+	 * Returns the list of display name choices.
+	 *
+	 * @since 2.5
+	 *
+	 * @return array
+	 */
 	public function get_display_name_choices() {
-
-		$display_names = $this->get_display_names();
-		$choices       = array();
-
-		$choices[] =  array(
-			'label' => sprintf( '&mdash; %s &mdash;', esc_html__( 'Preserve current display name', 'gravityflow'  ) ),
-			'value' => ''
-		);
-
-		foreach ( $display_names as $value => $label ) {
-			$choices[] = array(
-				'label' => $label,
-				'value' => $value
-			);
-		}
-
-		return $choices;
-	}
-
-	public function get_display_names() {
-
-		$choices   = array();
-
-		$choices['_preserve_display_name'] = sprintf( '&mdash; %s &mdash;', esc_html__( 'Preserve current display name', 'gravityflow'  ) );
+		$display_name_choices = array();
+		$choices = array();
 
 		$choices['nickname']  = '{nickname}';
 		$choices['username']  = '{username}';
@@ -245,11 +322,33 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 		$choices['firstlast'] = '{first name} {last name}';
 		$choices['lastfirst'] = '{last name} {first name}';
 
-		return $choices;
 
+
+		foreach ( $choices as $value => $label ) {
+			$display_name_choices[] = array(
+				'label' => $label,
+				'value' => $value
+			);
+		}
+
+		$display_name_choices[] =  array(
+			'label' => sprintf( '&mdash; %s &mdash;', esc_html__( 'Preserve current display name', 'gravityflow'  ) ),
+			'value' => ''
+		);
+
+		return $display_name_choices;
 	}
 
+	/**
+	 * Returns the roles as choices.
+	 *
+	 * @since 2.5
+	 *
+	 * @return array
+	 */
 	public function get_roles_as_choices() {
+
+		$choices = array();
 
 		$roles   = array_reverse( get_editable_roles() );
 
@@ -265,56 +364,12 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 	}
 
 	/**
-	 * Returns the "preserve current first name" choice.
+	 * Returns the user meta as choices.
 	 *
-	 * @since 3.8.2
-	 *
-	 * @return array
-	 */
-	public function get_extra_name_choice() {
-
-		$choices   = array();
-		$choices[] = array(
-			'label' =>  sprintf( '&mdash; %s &mdash;', esc_html__( 'Preserve current first name', 'gravityflow'  ) ),
-			'value' => ''
-		);
-
-
-		return $choices;
-	}
-
-	/**
-	 * Returns the "preserve current email" choice if this is an update type feed.
-	 *
-	 * @since 3.8.2
+	 * @since 2.5
 	 *
 	 * @return array
 	 */
-	public function get_extra_email_choice() {
-
-		$choices   = array();
-		$choices[] = array(
-			'label' =>  sprintf( '&mdash; %s &mdash;', esc_html__( 'Preserve current email', 'gravityflow'  ) ),
-			'value' => ''
-		);
-
-
-		return $choices;
-	}
-
-	public function get_extra_password_choices() {
-
-		$choices   = array();
-
-		$choices[] = array(
-			'label' => sprintf( '&mdash; %s &mdash;', esc_html__( 'Preserve current password', 'gravityflow'  ) ),
-			'value' => ''
-		);
-
-
-		return $choices;
-	}
-
 	public function get_user_meta_choices() {
 		global $wpdb;
 
@@ -395,6 +450,16 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 		return $choices;
 	}
 
+	/**
+	 * Callback for the settings to check whether a field can be mapped.
+	 *
+	 * @since 2.5
+	 *
+	 * @param $is_applicable_field
+	 * @param $field
+	 *
+	 * @return bool
+	 */
 	function is_applicable_field_for_field_select( $is_applicable_field, $field ) {
 
 		if ( rgobj( $field, 'multipleFiles' ) ) {
@@ -406,6 +471,8 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 
 	/**
 	 * Prepare value map.
+	 *
+	 * @since 2.5
 	 *
 	 * @return array
 	 */
@@ -419,6 +486,8 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 
 	/**
 	 * Returns the field map choices.
+	 *
+	 * @since 2.5
 	 *
 	 * @param array             $form
 	 * @param null|array|string $field_type
@@ -485,21 +554,21 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 				}
 
 				if ( is_array( $inputs ) && $field_is_valid_type && ! $exclude_field ) {
-					//If this is an address field, add full name to the list
+					// If this is an address field, add full name to the list
 					if ( $input_type == 'address' ) {
 						$fields[] = array(
 							'value' => $field->id,
 							'label' => GFCommon::get_label( $field ) . ' (' . esc_html__( 'Full', 'gravityflow' ) . ')',
 						);
 					}
-					//If this is a name field, add full name to the list
+					// If this is a name field, add full name to the list
 					if ( $input_type == 'name' ) {
 						$fields[] = array(
 							'value' => $field->id,
 							'label' => GFCommon::get_label( $field ) . ' (' . esc_html__( 'Full', 'gravityflow' ) . ')',
 						);
 					}
-					//If this is a checkbox field, add to the list
+					// If this is a checkbox field, add to the list
 					if ( $input_type == 'checkbox' ) {
 						$fields[] = array(
 							'value' => $field->id,
@@ -521,6 +590,189 @@ class Gravity_Flow_Step_Update_User extends Gravity_Flow_Step {
 
 		return $fields;
 	}
+
+	/**
+	 * Sets the user properties.
+	 *
+	 * @since 2.5
+	 *
+	 * @param WP_User $user
+	 */
+	public function set_user_properties( $user ) {
+
+		$entry = $this->get_entry();
+
+		$standard_meta = array(
+			'first_name',
+			'last_name',
+			'nickname',
+		);
+
+		$form = $this->get_form();
+
+		$dirty = false;
+
+		foreach ( $standard_meta as $meta_key ) {
+			$this->log_debug( sprintf( 'Processing meta item: %s', $meta_key ) );
+
+			if ( ! $this->{$meta_key} ) {
+				$this->log_debug( 'Meta item is empty. Skipping it.' );
+				continue;
+			}
+
+			$this->{$meta_key};
+			$value = gravity_flow()->get_field_value( $form, $entry, $this->{$meta_key} );
+			$this->log_debug( sprintf( 'Meta item mapped to field: %s; Value: %s', $meta_key, $value ) );
+
+			$result = update_user_meta( $user->ID, $meta_key, $value );
+			$this->log_debug( sprintf( 'Result: %s', var_export( (bool) $result, 1 ) ) );
+
+			$dirty = true;
+		}
+
+		if ( $this->display_name ) {
+			$display_name = $this->get_display_name( $user );
+			$this->update_user_property( $user->ID, 'display_name', $display_name );
+			$dirty = true;
+		}
+
+		if ( $this->email ) {
+			$value = gravity_flow()->get_field_value( $form, $entry, $this->email );
+			$this->update_user_property( $user->ID, 'user_email', $value );
+			$dirty = true;
+		}
+
+		if ( $this->password ) {
+			$value = gravity_flow()->get_field_value( $form, $entry, $this->password );
+			$this->update_user_property( $user->ID, 'user_pass', $value );
+			$dirty = true;
+		}
+
+		if ( is_array( $this->user_meta ) || ! empty( $this->user_meta ) ) {
+			foreach ( $this->user_meta as $meta_key => $meta_item ) {
+				$is_custom_value = $meta_item['value'] == 'gf_custom';
+
+				$meta_key = $meta_item['key'] == 'gf_custom' ? $meta_item['custom_key'] : $meta_item['key'];
+				$meta_value = $is_custom_value ? $meta_item['custom_value'] : $meta_item['value'];
+
+				$this->log_debug( sprintf( 'Adding meta item: %s', $meta_key ) );
+
+				// skip empty meta items
+				if ( ! $meta_key || ! $meta_value ) {
+					$this->log_debug( 'Meta item is empty. Skipping it.' );
+					continue;
+				}
+
+				if ( $is_custom_value ) {
+					$value = GFCommon::replace_variables( $meta_value, $form, $entry, false, false, false, 'text' );
+					$value = do_shortcode( $value );
+				} else {
+					$value = gravity_flow()->get_field_value( $form, $entry, $meta_value );
+					$this->log_debug( sprintf( 'Meta item mapped to field: %s; value: %s', $meta_value, $value ) );
+				}
+
+				if ( $value && in_array( $meta_key, array( 'user_url', 'user_nicename' ) ) ) {
+					$result = $this->update_user_property( $user->ID, $meta_key, $value );
+				} elseif ( rgblank( $value ) ) {
+					$result = delete_user_meta( $user->ID, $meta_key );
+				} else {
+					$result = update_user_meta( $user->ID, $meta_key, $value );
+				}
+
+				$this->log_debug( sprintf( 'Result: %s', var_export( (bool) $result, 1 ) ) );
+
+				$dirty = true;
+			}
+		}
+
+
+		if ( in_array( $this->roles_action, array( 'replace', 'add' ) ) ) {
+
+			// Update roles
+
+			if ( $this->roles_action == 'replace' ) {
+				foreach ( $user->roles as $role ) {
+					$user->remove_role( $role );
+				};
+			}
+
+			foreach ( $this->roles as $new_role ) {
+				$user->add_role( $new_role );
+			}
+
+			$dirty = true;
+		}
+
+		if ( $dirty ) {
+			update_user_meta( $user->ID, '_gravityflow-update-entry-id', $entry['id'] );
+		}
+	}
+
+	/**
+	 * Returns the display name for the given user according to the format defined in the settings.
+	 *
+	 * @since 2.5
+	 *
+	 * @param $user
+	 *
+	 * @return string
+	 */
+	public function get_display_name( $user ) {
+
+		$display_format = $this->display_name;
+
+		switch( $display_format ) {
+			case 'firstname':
+				$display_name = $user->first_name;
+				break;
+			case 'lastname':
+				$display_name = $user->last_name;
+				break;
+			case 'firstlast':
+				$display_name = $user->first_name . ' ' . $user->last_name;
+				break;
+			case 'lastfirst':
+				$display_name = $user->last_name . ' ' . $user->first_name;
+				break;
+			case 'nickname':
+				$display_name = $user->nickname;
+				break;
+			default:
+				$display_name = $user->user_login;
+				break;
+		}
+
+		return $display_name;
+	}
+
+	/**
+	 * Updates a user property
+	 *
+	 * @since 2.5
+	 *
+	 * @param $user_id
+	 * @param $prop_key
+	 * @param $prop_value
+	 *
+	 * @return bool|int|WP_Error
+	 */
+	public function update_user_property( $user_id, $prop_key, $prop_value ) {
+
+		if ( ! $user_id ) {
+			return false;
+		}
+
+		$user = new WP_User( $user_id );
+
+		$new_data            = new stdClass();
+		$new_data->ID        = $user->ID;
+		$new_data->$prop_key = $prop_value;
+
+		$result = wp_update_user( get_object_vars( $new_data ) );
+
+		return $result;
+	}
+
 }
 
 Gravity_Flow_Steps::register( new Gravity_Flow_Step_Update_User() );
