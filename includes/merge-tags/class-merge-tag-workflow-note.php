8ee3d837bda7a61ec4e3321a7ce3b17998977785
Idealien <jamie@idealienstudios.com>
2019-02-02 12:57:10 -0500

    Revise call to format_note from workflow-note merge tag

--- includes/merge-tags/class-merge-tag-workflow-note.php
+++ includes/merge-tags/class-merge-tag-workflow-note.php
@@ -77,7 +77,7 @@ class Gravity_Flow_Merge_Tag_Workflow_Note extends Gravity_Flow_Merge_Tag {
 						$name = $a['display_name'] ? self::get_assignee_display_name( $note['assignee_key'] ) : '';
 						$date = $a['display_date'] ? Gravity_Flow_Common::format_date( $note['timestamp'], '', false, true ) : '';
 
-						$replacement_array[] = self::format_note( $note['value'], $name, $date );
+						$replacement_array[] = Gravity_Flow_Common::format_note( $note['value'], $name, $date );
 					}
 
 					$replacement = $this->format_value( implode( "\n\n", $replacement_array ) );
