0fae35d1e8588d01207fcc7f4ceb1e475de7c0d9
Saif Sultan <saif@gravityflow.io>
2020-05-19 18:29:36 +0530

    ES5 - reverted to var from const

--- js/step-approval.js
+++ js/step-approval.js
@@ -1,15 +1,15 @@
 (function(){
-      const buttons = document.getElementsByClassName('gravityflow-action-buttons')[0].getElementsByTagName('button');
+      var buttons = document.getElementsByClassName('gravityflow-action-buttons')[0].getElementsByTagName('button');
   
       buttons[0].addEventListener('click', function(e) {
-        const approveConfirm = confirm( gravityflow_approval_confirmation_prompts.approveMessage );
+        var approveConfirm = confirm( gravityflow_approval_confirmation_prompts.approveMessage );
         if ( !approveConfirm ) {
           e.preventDefault();
         }
       });
   
       buttons[1].addEventListener('click', function(e) {
-        const rejectConfirm = confirm( gravityflow_approval_confirmation_prompts.rejectMessage );
+        var rejectConfirm = confirm( gravityflow_approval_confirmation_prompts.rejectMessage );
         if ( !rejectConfirm ) {
           e.preventDefault();
         }
