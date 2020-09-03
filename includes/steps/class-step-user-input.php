be9fbda7e988c2e9b47477430189b239e2ac239a
saifsultanc <saifraikkonen@gmail.com>
2020-09-03 23:41:52 +0530

    Fix Issue with Post Field on User Input Step

--- includes/steps/class-step-user-input.php
+++ includes/steps/class-step-user-input.php
@@ -1566,7 +1566,7 @@ class Gravity_Flow_Step_User_Input extends Gravity_Flow_Step {
 	 * @param int          $post_id The ID of the post created from the current entry.
 	 */
 	public function set_post_tags( $value, $post_id ) {
-		$post_tags = array( $value ) ? array_values( $value ) : explode( ',', $value );
+		$post_tags = is_array( $value ) ? array_values( $value ) : explode( ',', $value );
 
 		wp_set_post_tags( $post_id, $post_tags, false );
 	}
