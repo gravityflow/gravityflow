98c573b5d1e596ae738a29b1aa0256bda1be4583
speerface <aaron@rocketgenius.com>
2021-03-21 15:39:19 -0500

    Add in arch for ajax

--- includes/ajax/class-endpoint.php
+++ includes/ajax/class-endpoint.php
@@ -4,7 +4,57 @@ namespace Gravity_Flow\Gravity_Flow\Ajax;
 
 abstract class Endpoint {
 
+	protected $response_factory;
 
+	protected $data = array();
+	protected $required_args = array();
+	protected $optional_args = array();
+
+	public function __construct( Config $config, Response_Factory $factory ) {
+		$this->response_factory = $factory;
+
+		foreach ( $config->args() as $arg ) {
+			$name = $arg['name'];
+
+			if ( $arg['required'] ) {
+				$this->required_args[ $name ] = $arg;
+			} else {
+				$this->optional_args[ $name ] = $arg;
+			}
+		}
+	}
+
+	public function delegate() {
+		$posted_values = $_POST;
+
+		foreach ( $this->required_args as $rname => $config ) {
+			if ( ! isset( $posted_values[ $rname ] ) ) {
+				return new \WP_Error( 'Required argument ' . $rname . ' not set.' );
+			}
+
+			$this->add_values_to_data( $rname, $config );
+		}
+
+		foreach ( $this->optional_args as $oname => $config ) {
+			if ( ! isset( $posted_values[ $oname ] ) ) {
+				continue;
+			}
+
+			$this->add_values_to_data( $oname, $config );
+		}
+
+		return $this->handle();
+	}
+
+	protected function add_values_to_data( $name, $config ) {
+		$value = $_POST[ $rname ];
+
+		if ( is_callable( $config['sanitization'] ) ) {
+			$value = call_user_func( $config['sanitization'], $value );
+		}
+
+		$this->data[ $oname ] = $value;
+	}
 
 	abstract protected function handle();
 
--- includes/ajax/class-response.php
+++ includes/ajax/class-response.php
@@ -1,1 +1,26 @@
 <?php
+
+namespace Gravity_Flow\Gravity_Flow\Ajax;
+
+class Response {
+
+	private $data;
+	private $code;
+	private $return_strategy;
+
+	public function __construct( $data, $code, $return_strategy ) {
+		$this->data            = $data;
+		$this->code            = $code;
+		$this->return_strategy = $return_strategy;
+	}
+
+	public function response() {
+		if ( $code === 200 ) {
+			$this->return_strategy->success( $data );
+			return;
+		}
+
+		$this->return_strategy->error( $data, $code );
+	}
+
+}
