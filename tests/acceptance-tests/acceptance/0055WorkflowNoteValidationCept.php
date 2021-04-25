c39f8fd2ab829e99e24d46791653b46c33e75274
Idealien <jamie@idealienstudios.com>
2021-04-25 12:08:53 -0400

    Revise acceptance test to cover true empty and parsed empty note values

--- tests/acceptance-tests/acceptance/0055WorkflowNoteValidationCept.php
+++ tests/acceptance-tests/acceptance/0055WorkflowNoteValidationCept.php
@@ -26,31 +26,48 @@ $I->seeInCurrentUrl( '/wp-admin/' );
 $I->amOnWorkflowPage( 'Inbox' );
 $I->click( '0055 Workflow Note Validation' );
 
+//Approval - Attempt with an empty note.
 $I->waitForText( '0055 Workflow Note Validation : Entry # ', 3 );
 $I->see( '0055 Workflow Note Validation : Entry # ' );
 $I->see( 'Approval (Pending Approval)' );
-$I->fillField( 'Note', ' ' );
+$I->fillField( 'Note', '' );
 $I->click( 'Approve' );
-
 $I->waitForText( 'There was a problem while updating your form.', 3 );
 $I->see( 'There was a problem while updating your form.' );
 $I->see( 'A note is required' );
+
+//Approval - Attempt with a single space. 
+$I->fillField( 'Note', ' ' );
+$I->click( 'Approve' );
+$I->waitForText( 'There was a problem while updating your form.', 3 );
+$I->see( 'There was a problem while updating your form.' );
+$I->see( 'A note is required' );
+
+//Approval - Attempt with valid note.
 $I->fillField( 'Note', 'Approval note' );
 $I->click( 'Approve' );
-
 $I->waitForText( 'Entry Approved', 3 );
 $I->see( 'Entry Approved' );
 $I->see( 'Note: Approval note' );
-$I->see( 'User Input (Pending Input)' );
-$I->fillField( 'Note', ' ' );
-$I->click( 'Submit' );
 
+//User Input - Attempt with an empty note.
+$I->see( 'User Input (Pending Input)' );
+$I->fillField( 'Note', '' );
+$I->click( 'Submit' );
 $I->waitForText( 'There was a problem while updating your form.', 3 );
 $I->see( 'There was a problem while updating your form.' );
 $I->see( 'A note is required' );
+
+//User Input - Attempt with a single space.
+$I->fillField( 'Note', ' ' );
+$I->click( 'Submit' );
+$I->waitForText( 'There was a problem while updating your form.', 3 );
+$I->see( 'There was a problem while updating your form.' );
+$I->see( 'A note is required' );
+
+//User Input - Attempt with valid note.
 $I->fillField( 'Note', 'User Input note' );
 $I->click( 'Submit' );
-
 $I->waitForText( 'Entry updated and marked complete.', 3 );
 $I->see( 'Entry updated and marked complete.' );
 $I->see( 'Note: User Input note' );
