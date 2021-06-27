2f5142344ffa550dae6d231a6db7b35f70baa260
speerface <aaron@rocketgenius.com>
2021-06-27 14:23:00 -0500

    Update prefs to be per-table based

--- includes/inbox/class-preferences.php
+++ includes/inbox/class-preferences.php
@@ -5,7 +5,7 @@ namespace Gravity_Flow\Gravity_Flow\Inbox;
 class Preferences {
 
 	public function get_setting( $key, $id, $view = 'inbox', $default ) {
-		$value = $this->get_setting_from_user( $key, $id );
+		$value = $this->get_setting_from_user( $key, $id, $view );
 
 		if ( $value !== false && $value !== 0 && empty( $value ) ) {
 			$value = $this->get_setting_from_view( $key, $view );
@@ -18,52 +18,57 @@ class Preferences {
 		return $value;
 	}
 
-	public function save_setting( $key, $id, $value, $type = 'view' ) {
-		$method = sprintf( 'save_setting_for_' . $type );
-		$this->$method( $key, $id, $value );
-	}
-
-	public function get_setting_from( $type, $key, $id, $default ) {
+	public function save_setting( $key, $id, $value, $view = 'inbox', $type = 'view' ) {
 		switch ( $type ) {
 			case 'user':
-				return $this->get_setting_from_user( $key, $id );
+				return $this->save_setting_for_user( $key, $id, $view, $value );
 			case 'view':
-				return $this->get_setting_from_view( $key, $id );
+			default:
+				return $this->save_setting_for_view( $key, $id, $value );
+		}
+	}
+
+	public function get_setting_from( $type, $key, $id_or_view, $default ) {
+		switch ( $type ) {
+			case 'user':
+				return $this->get_setting_from_user( $key, $id_or_view );
+			case 'view':
+				return $this->get_setting_from_view( $key, $id_or_view );
 			default:
 				return $default;
 		}
 	}
 
-	private function get_setting_from_user( $key, $id ) {
-		$setting_key = $this->get_setting_key( $key );
+	private function get_setting_from_user( $key, $id, $view ) {
+		$setting_key = $this->get_setting_key( $key, $view );
 
 		return get_user_meta( $id, $setting_key, true );
 	}
 
-	private function get_setting_from_view( $key, $id ) {
-		$setting_key = $this->get_setting_key( $key, $id );
+	private function get_setting_from_view( $key, $view ) {
+		$setting_key = $this->get_setting_key( $key, $view );
 
 		return get_option( $setting_key );
 	}
 
-	private function save_setting_for_user( $key, $id, $value ) {
-		$setting_key = $this->get_setting_key( $key );
+	private function save_setting_for_user( $key, $id, $view, $value ) {
+		$setting_key = $this->get_setting_key( $key, $view );
 
 		return update_user_meta( $id, $setting_key, $value );
 	}
 
-	private function save_setting_for_view( $key, $id, $value ) {
-		$setting_key = $this->get_setting_key( $key, $id );
+	private function save_setting_for_view( $key, $view, $value ) {
+		$setting_key = $this->get_setting_key( $key, $view );
 
 		return update_option( $setting_key, $value );
 	}
 
-	private function get_setting_key( $key, $id = false ) {
-		if ( $id === false ) {
+	private function get_setting_key( $key, $view = false ) {
+		if ( $view === false ) {
 			return sprintf( 'gflow_setting_%s', $key );
 		}
 
-		return sprintf( 'gflow_setting_%s_%s', $key, $id );
+		return sprintf( 'gflow_setting_%s_%s', $key, $view );
 	}
 
 }
